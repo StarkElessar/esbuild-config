66fab9d88173f43f636ab5885ac07160abeefb34
stark <serj.elessar@gmail.com>
2024-08-16 22:44:19 +0300

    fix: compile package

--- types/types.d.ts
+++ types/types.d.ts
@@ -5,3 +5,9 @@ export interface DefineConfig {
 	outdir: string;
 	tsconfig?: string;
 }
+
+type DefineConfig = (externalConfig: DefineConfig) => Promise<void>;
+
+export declare module 'esbuild-config' {
+	export const defineConfig: DefineConfig;
+}
